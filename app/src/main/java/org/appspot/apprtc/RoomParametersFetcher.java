88f18b8142392ec683c6df4db64d004632b60af7
Piasy <xz4215@gmail.com>
2017-06-17 14:04:33 +0800

    Fix ICE server request 404

--- app/src/main/java/org/appspot/apprtc/RoomParametersFetcher.java
+++ app/src/main/java/org/appspot/apprtc/RoomParametersFetcher.java
@@ -165,7 +165,7 @@ public class RoomParametersFetcher {
     LinkedList<PeerConnection.IceServer> turnServers = new LinkedList<PeerConnection.IceServer>();
     Log.d(TAG, "Request TURN from: " + url);
     HttpURLConnection connection = (HttpURLConnection) new URL(url).openConnection();
-    connection.setDoOutput(true);
+    //connection.setDoOutput(true);
     connection.setRequestProperty("REFERER", "https://appr.tc");
     connection.setConnectTimeout(TURN_HTTP_TIMEOUT_MS);
     connection.setReadTimeout(TURN_HTTP_TIMEOUT_MS);
